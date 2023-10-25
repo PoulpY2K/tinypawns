1eb279859f70a9b7aadb41faaca4deaaf2c69746
Jérémy Laurent <poulpy2k@gmail.com>
2023-10-25 13:23:56 +0200

    feat(ruletile): add custom advanced rule tile script for customization purposes

---
+++ Assets/Scripts/AdvancedRuleTile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "RT_Advanced", menuName = "Advanced Rule Tile")]
+public class AdvancedRuleTile : RuleTile
+{
+    public Color color = Color.white;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        base.GetTileData(position, tilemap, ref tileData);
+
+        tileData.color = color;
+        tileData.flags = TileFlags.LockColor;
+    }
+}
